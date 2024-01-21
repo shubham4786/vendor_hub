40550e93561bde172eb9d9a8d4f4e93dcb3ca1ee
shubham4786 <shubhambhanarkar4786@gmail.com>
2024-01-21 06:27:59 +0530

    removed next-auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 export default function Home() {
   const router = useRouter();
   // const session = useSession();
